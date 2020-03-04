d329ae753a77345d81a7570bdf8815ce35b122a4
Isaac <ivcafe413@gmail.com>
2020-03-03 20:04:47 -0600

    Initial Commit, empty mod running with correct metadata

---
+++ src/main/java/technology/vagrant/vagrantaerospace/VagrantAerospace.java
@@ -0,0 +1,13 @@
+package technology.vagrant.vagrantaerospace;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VagrantAerospace.MODID)
+public class VagrantAerospace {
+    public static final String MODID = "vagrantaerospace";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+}
