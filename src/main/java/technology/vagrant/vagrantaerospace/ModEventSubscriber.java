1008e4fb3f3fc40dec5ddfe42e54deb4c37ae5d8
Isaac <ivcafe413@gmail.com>
2020-03-03 21:04:30 -0600

    Added registry event subscriber, sample model/texture

---
+++ src/main/java/technology/vagrant/vagrantaerospace/ModEventSubscriber.java
@@ -0,0 +1,26 @@
+package technology.vagrant.vagrantaerospace;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@EventBusSubscriber(modid = VagrantAerospace.MODID,
+    bus = EventBusSubscriber.Bus.MOD)
+public class ModEventSubscriber {
+    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(
+            setup(new Item(new Item.Properties()), "blast_furnace")
+        );
+    }
+
+    public static <T extends IForgeRegistryEntry<T>>T setup(final T entry, final String name) {
+        return setup(entry, new ResourceLocation(VagrantAerospace.MODID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>>T setup(final T entry, final ResourceLocation registryName) {
+        entry.setRegistryName(registryName);
+        return entry;
+    }
+}
