672828ec889fdd8467765ab214097a91505ca692
Isaac Vargas <ivargas@affirma.com>
2020-03-31 11:36:25 -0500

    Think I fixed the double block placement

--- src/main/java/technology/vagrant/aerocraftmod/items/WideBlockItem.java
+++ src/main/java/technology/vagrant/aerocraftmod/items/WideBlockItem.java
@@ -34,64 +34,66 @@ public class WideBlockItem extends BlockItem {
 
     @Override
     public ActionResultType tryPlace(@Nullable BlockItemUseContext context) {
-        //Overriding BlockItem#tryPlace(BlockItemUseContext context)
-        //to account for any generic 2-wide block
+        // Overriding BlockItem#tryPlace(BlockItemUseContext context)
+        // to account for any generic 2-wide block
 
-        //BlockItemUseContext blockItemUseContext = this.getBlockItemUseContext(context);
-        //I don't think the previous line is necessary, looking at how BlockItem#getBlockItemUseContext is implemented
-        //Also we're re-arranging this so that the null-check happens first
-        if(context == null) {
+        // BlockItemUseContext blockItemUseContext =
+        // this.getBlockItemUseContext(context);
+        // I don't think the previous line is necessary, looking at how
+        // BlockItem#getBlockItemUseContext is implemented
+        // Also we're re-arranging this so that the null-check happens first
+        if (context == null) {
             return ActionResultType.FAIL;
         }
 
-        if(!context.canPlace()) {
-            //If the block at targeted blockitem use can be replaced with new block (i.e. AIR)
+        if (!context.canPlace()) {
             return ActionResultType.FAIL;
         }
 
-        //Should be default states
         BlockState blockState = this.getStateForPlacement(context);
-        if(blockState == null) {
+        if (blockState == null) {
             return ActionResultType.FAIL;
         }
 
-        //Need to start a check if BOTH blocks can be placed
-        //Since this is item placement I just have to check for adjacency
+        // Need to start a check if BOTH blocks can be placed
+        // Since this is item placement I just have to check for adjacency
         Direction placementDirection = context.getPlacementHorizontalFacing().getOpposite();
         blockState = blockState.with(HorizontalBlock.HORIZONTAL_FACING, placementDirection);
         LOGGER.debug("context placement state facing:{}", blockState.get(HorizontalBlock.HORIZONTAL_FACING));
-        
+
         BlockPos adjPos1;
-        switch(placementDirection) {
-            //Facing east, check north-south
+        switch (placementDirection) {
+            // Facing east, check north-south
             case EAST:
                 adjPos1 = context.getPos().offset(Direction.NORTH);
                 break;
-            //Facing west, check south-north
+            // Facing west, check south-north
             case WEST:
-            adjPos1 = context.getPos().offset(Direction.SOUTH);
+                adjPos1 = context.getPos().offset(Direction.SOUTH);
                 break;
-            //Facing south, check east-west
+            // Facing south, check east-west
             case SOUTH:
-            adjPos1 = context.getPos().offset(Direction.EAST);
+                adjPos1 = context.getPos().offset(Direction.EAST);
                 break;
-            //Facing north, check west-east
+            // Facing north, check west-east
             case NORTH:
             default:
                 adjPos1 = context.getPos().offset(Direction.WEST);
                 break;
         }
         BlockState adjBlockState1 = context.getWorld().getBlockState(adjPos1);
-        if(!adjBlockState1.isReplaceable(context)) {
+        if (!adjBlockState1.isReplaceable(context)) {
             return ActionResultType.FAIL;
         }
 
-        if(!this.placeBlock(context, adjBlockState1)) {
+        adjBlockState1 = this.getStateForPlacement(context);
+        if (!this.placeBlock(context, adjBlockState1, adjPos1)) {
             return ActionResultType.FAIL;
         }
 
         if (!this.placeBlock(context, blockState)) {
-            //TODO: need to roll back placement of the other one if it fails
+            // TODO: need to roll back placement of the other one if it fails
+            // Can overload this#placeBlock to do all the setBlockState logic
             return ActionResultType.FAIL;
         }
 
@@ -107,42 +109,49 @@ public class WideBlockItem extends BlockItem {
             this.onBlockPlaced(blockpos, world, playerentity, itemstack, blockstate1);
             block.onBlockPlacedBy(world, blockpos, blockstate1, playerentity, itemstack);
             if (playerentity instanceof ServerPlayerEntity) {
-               CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity)playerentity, blockpos, itemstack);
+                CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity) playerentity, blockpos, itemstack);
             }
-         }
-        
-         SoundType soundtype = blockstate1.getSoundType(world, blockpos, context.getPlayer());
-         world.playSound(playerentity, blockpos, this.getPlaceSound(blockstate1, world, blockpos, context.getPlayer()), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-         itemstack.shrink(1);
-         return ActionResultType.SUCCESS;
+        }
+
+        SoundType soundtype = blockstate1.getSoundType(world, blockpos, context.getPlayer());
+        world.playSound(playerentity, blockpos, this.getPlaceSound(blockstate1, world, blockpos, context.getPlayer()),
+                SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+        itemstack.shrink(1);
+        return ActionResultType.SUCCESS;
     }
 
-    private BlockState func_219985_a(BlockPos p_219985_1_, World p_219985_2_, ItemStack p_219985_3_, BlockState p_219985_4_) {
+    private boolean placeBlock(BlockItemUseContext context, BlockState state, BlockPos pos) {
+        return context.getWorld().setBlockState(pos, state, 11);
+    }
+
+    private BlockState func_219985_a(BlockPos p_219985_1_, World p_219985_2_, ItemStack p_219985_3_,
+            BlockState p_219985_4_) {
         BlockState blockstate = p_219985_4_;
         CompoundNBT compoundnbt = p_219985_3_.getTag();
         if (compoundnbt != null) {
-           CompoundNBT compoundnbt1 = compoundnbt.getCompound("BlockStateTag");
-           StateContainer<Block, BlockState> statecontainer = p_219985_4_.getBlock().getStateContainer();
-  
-           for(String s : compoundnbt1.keySet()) {
-              IProperty<?> iproperty = statecontainer.getProperty(s);
-              if (iproperty != null) {
-                 String s1 = compoundnbt1.get(s).getString();
-                 blockstate = func_219988_a(blockstate, iproperty, s1);
-              }
-           }
-        }
-  
-        if (blockstate != p_219985_4_) {
-           p_219985_2_.setBlockState(p_219985_1_, blockstate, 2);
-        }
-  
-        return blockstate;
-     }
+            CompoundNBT compoundnbt1 = compoundnbt.getCompound("BlockStateTag");
+            StateContainer<Block, BlockState> statecontainer = p_219985_4_.getBlock().getStateContainer();
 
-     private static <T extends Comparable<T>> BlockState func_219988_a(BlockState p_219988_0_, IProperty<T> p_219988_1_, String p_219988_2_) {
+            for (String s : compoundnbt1.keySet()) {
+                IProperty<?> iproperty = statecontainer.getProperty(s);
+                if (iproperty != null) {
+                    String s1 = compoundnbt1.get(s).getString();
+                    blockstate = func_219988_a(blockstate, iproperty, s1);
+                }
+            }
+        }
+
+        if (blockstate != p_219985_4_) {
+            p_219985_2_.setBlockState(p_219985_1_, blockstate, 2);
+        }
+
+        return blockstate;
+    }
+
+    private static <T extends Comparable<T>> BlockState func_219988_a(BlockState p_219988_0_, IProperty<T> p_219988_1_,
+            String p_219988_2_) {
         return p_219988_1_.parseValue(p_219988_2_).map((p_219986_2_) -> {
-           return p_219988_0_.with(p_219988_1_, p_219986_2_);
+            return p_219988_0_.with(p_219988_1_, p_219986_2_);
         }).orElse(p_219988_0_);
-     }
+    }
 }
